07d906e5651dd8e1651b959fe2f2c041cd7e566b
Fathyaff <fathyaff@gmail.com>
2016-10-29 00:30:59 +0700

    Update Actions.java

--- Actions.java
+++ Actions.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import aima.core.agent.Action;
 import aima.core.search.framework.ActionsFunction;
+import aima.core.util.datastructure.XYLocation;
 
 /**
  * 
@@ -13,8 +16,18 @@ import aima.core.search.framework.ActionsFunction;
  */
 public class Actions implements ActionsFunction {
 
-	public Set<Action> actions(Object arg0) {
+	public Set<Action> actions(Object o) {
 		// TODO Auto-generated method stub
+		Map map = (Map) o;
+		Set<Action> actions = new LinkedHashSet<Action>();
+		
+		XYLocation tony = map.getTony();
+		
+		ArrayList<String> res = new ArrayList<String>();
+		
+		Set<XYLocation> notValidLocations = new LinkedHashSet<XYLocation>();
+		
+		
 		return null;
 	}
 
