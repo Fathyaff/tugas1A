3100432d0627f90d965af68f148fdaebb1b349fd
Fathyaff <fathyaff@gmail.com>
2016-10-29 00:10:20 +0700

    Create Game.java

--- Game.java
+++ Game.java
@@ -1,9 +1,11 @@
 import java.util.Iterator;
 import java.util.Properties;
+
 import aima.core.search.framework.Problem;
 import aima.core.search.framework.Search;
 import aima.core.search.framework.SearchAgent;
 import aima.core.search.uninformed.IterativeDeepeningSearch;
+import aima.core.util.datastructure.XYLocation;
 
 /**
  * 
@@ -18,7 +20,10 @@ public class Game {
 	public static final int IDS = 0;
 	public static final int ASTAR = 1;
 	
-	
+	private static final char TONY = 'T';
+	private static final char ITEM = 'I';
+	private static final char BARRIER = 'B';
+	private static final char NULL = 'N';
 	private Map map;
 	public Game(Map map) {
 		// TODO Auto-generated constructor stub
@@ -34,19 +39,12 @@ public class Game {
 			return "No Solution";
 		}
 		String output = "";
-		
+		System.out.println(strategy + "s");
 		switch(strategy){
 			case IDS: output = playIDS();break;
 			//case ASTAR: output = playASTAR();break;
 		}
 		return output;
-	}
-	/**
-	 * Game is playing in current strategy
-	 *
-	 */
-	public boolean solution(){
-		return false;
 	}
 	
 	private String playIDS() throws Exception{
@@ -57,10 +55,72 @@ public class Game {
 		Search search = new IterativeDeepeningSearch();
 		SearchAgent agent = new SearchAgent(problem, search);
 		String output = getPathCost(agent.getInstrumentation()) + "\n";
+		System.out.println("haaaii");
 		System.out.println(getNumberOfNodesExpanded(agent.getInstrumentation()));
 		
 		return output;
 	}
+	
+	/**
+	 * Game is playing in current strategy
+	 *
+	 */
+	private boolean solution(){
+		boolean solution = false;
+		char[][] map1 = new char[map.getRow()][map.getCol()];
+		for(int i = 0;i<map.getRow();i++){
+			for (int j = 0; j< map.getCol();j++){
+				XYLocation present = new XYLocation(i+1,j+1);
+				char a = NULL;
+				
+				if(present.equals(map.getTony())){
+					a = TONY;
+				}
+				for(XYLocation location : map.getItem()){
+					if(present.equals(location)) a = ITEM;
+				}
+				
+				for(XYLocation location : map.getBarrier()){
+					if(present.equals(location)) a = BARRIER;
+				}
+				
+				map1[i][j] = a;
+			}
+			solution = true;
+			for (XYLocation item : map.getItem()){
+				int x = item.getXCoOrdinate() -1;
+				int y = item.getYCoOrdinate() -1;
+				boolean[][] visited = new boolean[map.getRow()][map.getCol()];
+				boolean pathToItem = checkSolution(map1, visited, x,y,ITEM);
+				boolean pathToTony = checkSolution(map1, visited, x,y,TONY);
+				System.out.println("Path to item " + pathToItem);
+				System.out.println("Path to tony " + pathToTony);
+				
+				solution = pathToItem && pathToTony;
+				System.out.println("solution " + solution);
+				if(!solution) break;
+			}
+			
+		}
+		return solution;
+	}
+	
+	private boolean checkSolution(char[][] map1, boolean[][] visited, int x,
+			int y, char goal) {
+		// TODO Auto-generated method stub
+		if(validCoor(map1, x,y) || map1[x][y] == BARRIER || visited[x][y]){
+			return false;
+		}
+		if(map1[x][y] == goal){return true;}
+		visited[x][y] = true;
+		return checkSolution(map1, visited, x, y-1, goal) || checkSolution(map1, visited, x+1, y, goal)
+			|| checkSolution(map1, visited, x, y+1, goal) || checkSolution(map1, visited, x-1, y, goal);
+	}
+	
+	private boolean validCoor(char[][] map1, int  x, int y){
+		return x >= 0 && y >= 0 && x < map1.length && y < map1[0].length;
+	}
+	
 	
 	private String getPathCost(Properties properties) {
 	    String res = "";
